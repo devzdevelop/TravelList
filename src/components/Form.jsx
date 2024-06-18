f0b3d11241f00341337c7140a4e3e40db14e128e
devzdevelop <devbusinessinfluencer@gmail.com>
2024-06-18 17:03:52 -0500

    Made the form component able to save data from the form. Also, Implemented the line through capability based on whether pack is true or false.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,25 +4,34 @@ const Form = () => {
   const [description, setDescription] = useState('');
   const [quantity, setQuantity] = useState(1);
 
-  const [items, setItems] = useState([{
-    description: "cake",
-    quantity: 2
-  }]);
-
   function HandleInput ({target}) {
     setDescription(target.value);
   }
 
   function HandleSubmit (e) {
     e.preventDefault();
-    setItems((prev) => [...prev, {description: description, quantity: quantity}])
-    console.log(e);
+
+    if(!description) return;
+
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: Date.now(),
+    };
+    console.log(newItem);
+
+    setDescription('');
+    setQuantity(1);
   }
   return (
     <>
       <form className="add-form" onSubmit={HandleSubmit}>
         <h3>What do you need for your trip? 😍</h3>
-        <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
+        <select 
+          value={quantity} 
+          onChange={(e) => setQuantity(Number(e.target.value))}
+        >
           {
             Array.from({length: 20}, (_, i) => i + 1).map
             ((num) => (
@@ -32,7 +41,12 @@ const Form = () => {
             ))
           }
         </select>
-        <input onChange={HandleInput} type="select" placeholder="Item..." value={description}/>
+        <input 
+          onChange={HandleInput} 
+          type="select" 
+          placeholder="Item..." 
+          value={description}
+        />
         <button>Add</button>
       </form>     
     </>
