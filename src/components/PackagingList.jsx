9f7b45a323502e919f2538b9b4dbdc2591029592
devzdevelop <devbusinessinfluencer@gmail.com>
2024-06-20 09:46:55 -0500

    Implemented the sort functionality in the PackagingList component.

--- src/components/PackagingList.jsx
+++ src/components/PackagingList.jsx
@@ -1,4 +1,5 @@
 import Item from "./Item"
+import {useState} from 'react';
 //const initialItems = [
 //  { id: 1, description: "Passports", quantity: 2, packed: false },
 //  { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -6,16 +7,29 @@ import Item from "./Item"
 //];
 
 const PackagingList = ({items, onDeleteItem, onToggleItem}) => {
+  const [sortBy, setSortBy] = useState("input");
   
+  let sortedItems;
+  
+  if(sortBy === 'input') sortedItems = items;
+  if(sortBy === 'description') sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description));
+  if(sortBy === 'packed') sortedItems = items.slice().sort((a, b) => Number(a.packed) - Number(b.packed))
   return (
       <div className="list">
         <ul>
-          {items.map((item) => 
+          {sortedItems.map((item) => 
             <>
               <Item item={item} key={item.id} id={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem}/>
             </>
           )}
         </ul>
+        <div className="action">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by Input</option>
+          <option value="description">Sort by Description</option>
+          <option value="packed">Sort by Packed Status</option>
+        </select>
+      </div>
       </div>
   );
 }
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,6 +1,6 @@
 const Stats = ({items}) => {
   let packedItems = items.filter((item) => item.packed !== false).length;
-  let packedItemsPercentage = (packedItems / items.length) * 100; 
+  let packedItemsPercentage = (packedItems / items.length || 0) * 100; 
   return (
     <footer className="stats">
       <em>You have {items.length} items on your list, and you already packed {packedItems} ({packedItemsPercentage}%).</em>
